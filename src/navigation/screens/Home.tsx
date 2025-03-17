5dcde4259e652528a35771d2b57b3d070704dc4b
Will <wwspradlin@mail.roanoke.edu>
2025-03-17 18:34:50 -0400

    tring this

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -14,7 +14,7 @@ function Game(){
       {squares.map((row, rowIndex) => (
         <View key={rowIndex} style={{flexDirection: 'row'}}>
           {row.map((square, squareIndex) => (
-            <View key={squareIndex} style={{width: 50, height: 50, backgroundColor: square ? 'white' : 'black'}}/>
+            <View key={squareIndex} style={{width: 50, height: 50, backgroundColor: true ? 'white' : 'black'}}/>
           ))}
         </View>
     </View>
