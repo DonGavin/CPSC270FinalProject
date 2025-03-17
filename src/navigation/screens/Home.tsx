9fadaeb2a4b61549c6b39075f80eb20f73986f8d
Will <wwspradlin@mail.roanoke.edu>
2025-03-17 18:32:14 -0400

    testing colors

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,14 +1,22 @@
 import { Button, Text } from '@react-navigation/elements';
 import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
 
 function Title() {
   return <Text style={{ fontSize: 30 }}>Chess Game</Text>;
 } 
 
 function Game(){
+  const squaresNumber = 8;
+  const [squares, setSquares] = useState(Array(size).fill(null).map(()=>Array(size).fill(null).map(() => Math.random() < 0.5)))
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightblue', width: '100%'}}>
-      <Text>Game</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightblue', width: '50%'}}>
+      {squares.map((row, rowIndex) => (
+        <View key={rowIndex} style={{flexDirection: 'row'}}>
+          {row.map((square, squareIndex) => (
+            <View key={squareIndex} style={{width: 50, height: 50, backgroundColor: square ? 'white' : 'black'}}/>
+          ))}
+        </View>
     </View>
   )
 }
