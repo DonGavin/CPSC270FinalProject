1b35f745abb5d3435a475f44a8a16429fb15f7bb
Will <wwspradlin@mail.roanoke.edu>
2025-03-17 18:55:39 -0400

    testing change

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -15,7 +15,9 @@ function Game(){
       {squares.map((row, rowIndex) => (
         <View key={rowIndex} style={{flexDirection: 'row'}}>
           {row.map((square, squareIndex) => (
-            <View key={squareIndex} style={{width: `(${100/8})%`, height: `(${100/8})%`, backgroundColor: ((rowIndex+squareIndex)%2) == 1  ? 'white' : 'black'}}/>
+            <View key={squareIndex} style={{width: `(${100/8})%`, height: `(${100/8})%`, backgroundColor: ((rowIndex+squareIndex)%2) == 1  ? 'white' : 'black'}}>
+              <Text>{100/8}%</Text>
+            </View>
             
           ))}
         </View>
