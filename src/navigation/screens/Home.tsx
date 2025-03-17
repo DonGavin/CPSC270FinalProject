c8351fc245c716ea0cb60981b5fd49d40c20496e
Will <wwspradlin@mail.roanoke.edu>
2025-03-17 18:59:39 -0400

    test

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -11,11 +11,11 @@ function Game(){
   const [squares, setSquares] = useState(Array(squaresNumber).fill(null).map(()=>Array(squaresNumber).fill(null).map(() => Math.random() < 0.5)))
   const [color, setColor] = useState(true)
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightblue', width: '50%'}}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightblue', width: '100%', height:'100%'}}>
       {squares.map((row, rowIndex) => (
-        <View key={rowIndex} style={{flexDirection: 'row',height: `(${100/8})%`}}>
+        <View key={rowIndex} style={{flexDirection: 'row',flex: 1}}>
           {row.map((square, squareIndex) => (
-            <View key={squareIndex} style={{width: `(${100/8})%`, height: `(${100/8})%`, backgroundColor: ((rowIndex+squareIndex)%2) == 1  ? 'white' : 'black'}}>
+            <View key={squareIndex} style={{width: `(${100/squaresNumber})%`, height: `(${100/squaresNumber})%`, backgroundColor: ((rowIndex+squareIndex)%2) == 1  ? 'white' : 'black'}}>
               <Text>{100/8}%</Text>
             </View>
             
