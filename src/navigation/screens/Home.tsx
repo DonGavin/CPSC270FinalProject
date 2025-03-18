5ecb49f1e73e6151c498749693300415e5130994
DonGavin <gjd58168@gmail.com>
2025-03-18 14:44:18 -0400

    Added logic for piece selection with state to track selection of pieces

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,15 +1,42 @@
 import { Button, Text } from '@react-navigation/elements';
-import { StyleSheet, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+
+export function Piece({pieceId}){
+  // squares for each piece with their id as the value 
+  const [notSelected, pieceSelected] = useState({
+    pieces: {
+      1: {name: "Pawn", selected:false, currentPos: 0},
+      2: {name: "Knight", selected:false, currentPos: 0},
+      3: {name: "Rook", selected:false, currentPos:0},
+      4: {name: "Bishop", selected:false, currentPos: 0},
+      5: {name: "Queen", selected: false, currentPos: 0},
+      6: {name: "King", selected: false, currentPos:0}
+    }
+  });
+  const onSelected = (pieceId) => {
+    pieceSelected((prevState) => {
+      const updatedPieces = { ...prevState.pieces };
+      updatedPieces[pieceId].selected = true;
+      return {
+        ...prevState,
+        pieces: updatedPieces,
+      };
+    });
+    console.log(pieceId);
+    console.log(notSelected);
+  }
+  return(
+    <Pressable onPress={() => onSelected(pieceId)}>
+      <Text>{pieceId}</Text>
+    </Pressable>
+  )
+}  
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Text>Open up 'src/App.tsx' to start working on your app!</Text>
-      <Button screen="Profile" params={{ user: 'jane' }}>
-        Go to Profile
-      </Button>
-      <Button screen="Settings">Go to Settings</Button>
+      <Piece pieceId={2}/>  
     </View>
   );
 }
