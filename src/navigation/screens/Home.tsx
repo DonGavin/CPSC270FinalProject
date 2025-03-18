e81c3df674bd99267af05a0c25044a891e8cf328
Will <wwspradlin@mail.roanoke.edu>
2025-03-18 16:23:25 -0400

    so far

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,10 +1,23 @@
 import { Button, Text } from '@react-navigation/elements';
-import { StyleSheet, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 import { useState, useEffect } from 'react';
 
 function Title() {
   return <Text style={{ fontSize: 30 }}>Chess Game</Text>;
-} 
+}
+
+function Pawn() {
+  return (
+    <View style={{ width: 25, height: 25, backgroundColor: 'red' }}></View>
+  );
+}
+
+function Rook() {
+  [color, setColor] = useState(true)
+  return (
+    <View ><Pressable style={{ width: 25, height: 25, backgroundColor: color ? 'blue' : 'green' }} onPress={()=>{setColor(!color)}}></Pressable></View>
+  );
+}
 
 function Game(){
   const squaresNumber = 8;
@@ -15,7 +28,9 @@ function Game(){
       {squares.map((row, rowIndex) => (
         <View key={rowIndex} style={{flexDirection: 'row', flex: 1}}>
           {row.map((square, squareIndex) => (
-            <View key={squareIndex} style={{flex:1, aspectRatio: 1, backgroundColor: ((rowIndex+squareIndex)%2) == 1  ? 'green' : 'black'}}>
+            <View id={(rowIndex+1)*(squareIndex+1)} key={squareIndex} style={{flex:1, aspectRatio: 1, backgroundColor: ((rowIndex+squareIndex)%2) == 1  ? 'green' : 'black', justifyContent:'center', alignItems:'center'}} >
+              {(Math.floor((((rowIndex)*8)+(squareIndex))/8)) == 1 || (Math.floor((((rowIndex)*8)+(squareIndex))/8)) == 6 ? Pawn() : 
+              ((Math.floor((((rowIndex)*8)+(squareIndex))/8)) == 0 && squareIndex == 0) || ((Math.floor((((rowIndex)*8)+(squareIndex))/8)) == 0 && squareIndex == 7)  ? Rook() : null}
             </View>
             
           ))}
